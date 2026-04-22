712c2f66b85a728c8dc261903eb631d41679a4fd
thepoy <thepoy@163.com>
2026-04-22 16:10:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 
-export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
-  build: { sourcemap: true, target: "esnext" },
-  resolve: {
-    dedupe: ["solid-js", "solid-js/web", "solid-js/store"],
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [devtools(), solidPlugin(), tailwindcss()],
+    server: {
+      port: 3000,
+    },
+    build: { sourcemap: mode === "staging", target: "esnext" },
+    resolve: {
+      dedupe: ["solid-js", "solid-js/web", "solid-js/store"],
+    },
+  };
 });
